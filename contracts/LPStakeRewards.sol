5d3e5cca5e80a8dec5f94f7e5b8fd1a3b936b3d9
Abhik Banerjee <abhik.banerjee.1999@gmail.com>
2023-01-17 20:32:42 +0530

    Final Commit.

--- contracts/LPStakeRewards.sol
+++ contracts/LPStakeRewards.sol
@@ -35,12 +35,16 @@ contract LPStakeRewards is ERC1155URIStorage, Ownable, ReentrancyGuard {
     function fundContract(uint256 _amount) external onlyOwner {
         // Approve needs to be taken care of beforehand
         lpTokenContract.transferFrom(_msgSender(), address(this), _amount);
+        currentRewardPool += _amount;
     }
 
     function stake(uint256 _amount) external nonReentrant {
         require(currentRewardPool > 0, "Reward Pool Empty");
         address user = _msgSender();
-        require(stakingRegistry[user].level == 0, "You are not allowed to stake");
+        require(
+            stakingRegistry[user].level == 0,
+            "You are not allowed to stake"
+        );
 
         // For First Time Stakers
         if (stakingRegistry[user].amount == 0) {
@@ -59,9 +63,11 @@ contract LPStakeRewards is ERC1155URIStorage, Ownable, ReentrancyGuard {
     }
 
     // Returns if the user is eligible for an upgrade and if so then to which level
-    function _upgradeEligible(
-        address _user
-    ) internal view returns (bool, uint8) {
+    function _upgradeEligible(address _user)
+        internal
+        view
+        returns (bool, uint8)
+    {
         /*
         Rules:
         User Staked less than 10e18 Lp Tokens -> Upgrades in 30 days from L1 to L2 and in further 18 days from L2 to L3
@@ -130,73 +136,51 @@ contract LPStakeRewards is ERC1155URIStorage, Ownable, ReentrancyGuard {
         require(currentRewardPool > 0, "Reward Pool Empty");
         address userAddress = _msgSender();
         User memory user = stakingRegistry[userAddress];
+        uint256 transferAmount = 0;
 
         // User has atleast staked for 11 days
         if (user.level == 0 && user.time > 11 * 24 * 60 * 60) {
             if (user.amount < 10e18) {
                 // 0.01% reward if less than 10 tokens staked
-                lpTokenContract.transferFrom(
-                    address(this),
-                    userAddress,
-                    user.amount + (user.amount / 10000)
-                );
+                transferAmount = user.amount + (user.amount / 10000);
                 user.amount = 0;
             } else {
                 // 1.5% reward if less than 10 tokens staked
-                lpTokenContract.transferFrom(
-                    address(this),
-                    userAddress,
-                    user.amount + ((user.amount * 15) / 1000)
-                );
+                transferAmount = user.amount + ((user.amount * 15) / 1000);
                 user.amount = 0;
             }
         } else if (user.level == 1) {
             if (user.amount < 10e18) {
                 // 2% reward if less than 10 tokens staked
-                lpTokenContract.transferFrom(
-                    address(this),
-                    userAddress,
-                    user.amount + ((user.amount * 2) / 100)
-                );
+                transferAmount = user.amount + ((user.amount * 2) / 100);
                 user.amount = 0;
             } else {
                 // 3% reward if less than 10 tokens staked
-                lpTokenContract.transferFrom(
-                    address(this),
-                    userAddress,
-                    user.amount + ((user.amount * 3) / 100)
-                );
+                transferAmount = user.amount + ((user.amount * 3) / 100);
                 user.amount = 0;
             }
         } else if (user.level == 2) {
             if (user.amount < 10e18) {
                 // 3.5% reward if less than 10 tokens staked
-                lpTokenContract.transferFrom(
-                    address(this),
-                    userAddress,
-                    user.amount + ((user.amount * 35) / 1000)
-                );
+                transferAmount = user.amount + ((user.amount * 35) / 1000);
                 user.amount = 0;
             } else {
                 // 5% reward if less than 10 tokens staked
-                lpTokenContract.transferFrom(
-                    address(this),
-                    userAddress,
-                    user.amount + ((user.amount * 5) / 100)
-                );
+                transferAmount = user.amount + ((user.amount * 5) / 100);
                 user.amount = 0;
             }
         } else {
             // -50% as penalty if less than 10 tokens staked
-            lpTokenContract.transferFrom(
-                address(this),
-                userAddress,
-                user.amount - ((user.amount * 50) / 100)
-            );
+            transferAmount = user.amount - ((user.amount * 50) / 100);
             user.amount = 0;
         }
-
+        lpTokenContract.transferFrom(
+            address(this),
+            userAddress,
+            transferAmount
+        );
         stakingRegistry[userAddress] = user;
+        currentRewardPool -= transferAmount;
     }
 
     function _beforeTokenTransfer(
